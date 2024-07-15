6d1379af3b54a9ba4a29ed69a2340f27d061bf10
DenSad81 <tagil81@mail.ru>
2024-07-15 15:45:06 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -1,37 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
         Queue<int> purchases = new Queue<int>();
-        Random random = new Random();
         int quantityOfPurchases = 10;
         int minPrice = 10;
         int maxPrice = 100;
         int moneyInBankAccount = 0;
 
-        for (int i = 0; i < quantityOfPurchases; i++)
-            purchases.Enqueue(random.Next(minPrice, maxPrice));
+        purchases = FillQueue(quantityOfPurchases, minPrice, maxPrice);
 
         while (purchases.Count > 0)
         {
-            foreach (var item in purchases)
-                Console.WriteLine(item);
+            ShowQuenque(purchases);
 
             Console.WriteLine();
             int purchasePrice = purchases.Dequeue();
             Console.WriteLine("Цена за покупку: " + purchasePrice);
-            moneyInBankAccount += purchasePrice;
             Console.WriteLine("Количество денег на счету: " + moneyInBankAccount);
             Console.ReadKey();
+            moneyInBankAccount += purchasePrice;
             Console.Clear();
         }
 
         Console.WriteLine("Покупки закончились количество денег на счету: " + moneyInBankAccount);
     }
+
+    static Queue<int> FillQueue(int volume, int minValue, int maxValue)
+    {
+        Queue<int> tempQueue = new Queue<int>();
+        Random random = new Random();
+
+        for (int i = 0; i < volume; i++)
+            tempQueue.Enqueue(random.Next(minValue, maxValue));
+
+        return tempQueue;
+    }
+
+    static void ShowQuenque(Queue<int> tempQueue)
+    {
+        foreach (var item in tempQueue)
+            Console.WriteLine(item);
+    }
+
 }
